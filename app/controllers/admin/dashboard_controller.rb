b09fc5227df3456c72eed21feda37a937d5deed1
Daniel Lu <daniel_lu@hotmail.com>
2022-02-03 15:00:50 -0500

    add security password to admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with :name => ENV['MAX_SECURITY_USERNAME'], :password => ENV["MAX_SECURITY_PASSWORD"]
+  
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with :name => ENV['MAX_SECURITY_USERNAME'], :password => ENV["MAX_SECURITY_PASSWORD"]
+
   def index
     @products = Product.order(id: :desc).all
   end
